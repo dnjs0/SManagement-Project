0defc7ba704ba6547284ec2013b0e11fb03aeae3
dnjs0 <heatherray@naver.com>
2025-02-07 14:49:17 +0900

    f

---
+++ 10. 개인 업무 자료/자료수집/주요기술스택_insert.sql
@@ -0,0 +1,11 @@
+-- 주요기술스택
+INSERT INTO tech VALUES (1, '자바');
+INSERT INTO tech VALUES (2, '파이썬');
+INSERT INTO tech VALUES (3, 'AWS');
+INSERT INTO tech VALUES (4, '자바스크립트');
+INSERT INTO tech VALUES (5, '리액트');
+INSERT INTO tech VALUES (6, '도커');
+INSERT INTO tech VALUES (7, 'Kubernetes');
+INSERT INTO tech VALUES (8, 'SQL');
+INSERT INTO tech VALUES (9, 'TensorFlow');
+INSERT INTO tech VALUES (10, 'PyTorch');
